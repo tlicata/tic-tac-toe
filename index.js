dc129db60a897347be1f2aefb0c3d45366bdfd54
Tim Licata <tim@timothylicata.com>
2016-02-05 00:02:08 -0800

    get "Hello, World!" example working with react

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-document.write("Hello, World!");
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+var Hello = React.createClass({
+  render: function () {
+    return React.createElement("div", null, "Hello ", this.props.name);
+  }
+});
+
+ReactDOM.render(
+  React.createElement(Hello, {name: "World!"}),
+  document.getElementById("container")
+);
