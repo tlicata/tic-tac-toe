73a225bce6553f2419f009d6d64a5e20a31930e3
Tim Licata <tim@timothylicata.com>
2016-02-08 23:18:26 -0800

    add board js and css

--- app/board.js
+++ app/board.js
@@ -1,13 +1,57 @@
 var React = require("react");
+var Engine = require("./engine");
 
-var Hello = React.createClass({
+var Square = React.createClass({
+  handleClick: function (e) {
+    this.props.onPlay(this.props.position);
+  },
   render: function () {
+    var position = this.props.position;
+    var gameOver = this.props.game.isOver();
+
+    var classes = ["square", "square" + position];
+    if (Array.isArray(gameOver) && gameOver.includes(position)) {
+      classes.push("winner");
+    }
+    classes = classes.join(" ");
+
     return (
-      <div>
-        Hello
+      <div className={classes} onClick={this.handleClick}>
+        {this.props.game.playerAt(position)}
       </div>
     );
   }
 });
 
-module.exports = Hello;
+var Board = React.createClass({
+  getInitialState: function() {
+    return Engine.createNewGame();
+  },
+  handlePlay: function (square) {
+    this.setState(this.state.play(square));
+  },
+  render: function () {
+    var attrs = {game:this.state, onPlay:this.handlePlay};
+    return (
+      <div className="board">
+        <div className="row">
+          <Square position={0} {...attrs} />
+          <Square position={1} {...attrs} />
+          <Square position={2} {...attrs} />
+        </div>
+        <div className="row">
+          <Square position={3} {...attrs} />
+          <Square position={4} {...attrs} />
+          <Square position={5} {...attrs} />
+        </div>
+        <div className="row">
+          <Square position={6} {...attrs} />
+          <Square position={7} {...attrs} />
+          <Square position={8} {...attrs} />
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = Board;
