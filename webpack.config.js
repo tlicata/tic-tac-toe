1eb9fddaf0ae4f74aff0dd18474061f8540639d6
Tim Licata <tim@timothylicata.com>
2016-02-11 11:54:00 -0800

    add babel-polyfill in build process for css-loader
    i tried to run the build scripts on an ubuntu machine with an old
    install of node and hit an error due to Promise being undefined.
    
    https://github.com/webpack/css-loader/issues/144

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
 var path = require("path");
 
+// css-loader requires a recent version of node since they removed the
+// shim for Promise. Bring in babel-polyfill in case running on an old
+// system.
+// https://github.com/webpack/css-loader/issues/144
+require("babel-polyfill");
+
 module.exports = {
   entry: path.resolve(__dirname, "app/index.js"),
   output: {
